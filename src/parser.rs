3a9a93ed12f9ae72dbb9a889f937bb4e07a19023
coord.e <me@coord-e.com>
2018-08-07 17:08:00 +0900

    Add: Add a tests for litertals

--- src/parser.rs
+++ src/parser.rs
@@ -7,3 +7,29 @@ pub mod syntax {
 pub fn parse(x: &str) -> Result<Expression, syntax::ParseError> {
     syntax::expression(x)
 }
+
+#[test]
+fn test_number() {
+    assert_eq!(parse("0"), Ok(Expression::Number(0)))
+}
+
+#[test]
+fn test_multidigit_number() {
+    assert_eq!(parse("10"), Ok(Expression::Number(10)))
+}
+
+#[test]
+fn test_identifier() {
+    assert_eq!(parse("abc"), Ok(Expression::Identifier("abc".to_string())))
+}
+
+#[test]
+fn test_onechar_identifier() {
+    assert_eq!(parse("a"), Ok(Expression::Identifier("a".to_string())))
+}
+
+#[test]
+fn test_invalid_identifier() {
+    assert!(parse("_a").is_err());
+    assert!(parse("0a").is_err())
+}
