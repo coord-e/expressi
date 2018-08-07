319ca9dda4705d1b7ef3f077beb28d2abbf03576
coord.e <me@coord-e.com>
2018-08-07 16:40:33 +0900

    Change: Handle operators as an enum

--- src/expression.rs
+++ src/expression.rs
@@ -1,5 +1,22 @@
 #[derive(Clone, PartialEq, Eq, Debug)]
+pub enum Operator {
+    Add, Sub, Mul, Div, Unknown
+}
+
+impl Operator {
+    pub fn from_str(x: &str) -> Operator {
+        match x {
+            "+" => Operator::Add,
+            "-" => Operator::Sub,
+            "*" => Operator::Mul,
+            "/" => Operator::Div,
+            _   => Operator::Unknown
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
 pub enum Expression {
     Number(i64),
-    BinOp(String, Box<Expression>, Box<Expression>),
+    BinOp(Operator, Box<Expression>, Box<Expression>),
 }
