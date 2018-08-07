b27828525a5f585da903842031c870c8a898f053
coord.e <me@coord-e.com>
2018-08-07 18:41:37 +0900

    Change: Separate tests into tests mod

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,4 @@
 use expression::Expression;
-use expression::Operator;
 
 pub mod syntax {
     include!(concat!(env!("OUT_DIR"), "/syntax.rs"));
@@ -9,54 +8,60 @@ pub fn parse(x: &str) -> Result<Expression, syntax::ParseError> {
     syntax::expression(x)
 }
 
-#[test]
-fn test_number() {
-    assert_eq!(parse("0"), Ok(Expression::Number(0)))
-}
+#[cfg(test)]
+mod tests {
+    use expression::Operator;
+    use super::*;
 
-#[test]
-fn test_multidigit_number() {
-    assert_eq!(parse("10"), Ok(Expression::Number(10)))
-}
-
-#[test]
-fn test_identifier() {
-    assert_eq!(parse("abc"), Ok(Expression::Identifier("abc".to_string())))
-}
-
-#[test]
-fn test_onechar_identifier() {
-    assert_eq!(parse("a"), Ok(Expression::Identifier("a".to_string())))
-}
-
-#[test]
-fn test_invalid_identifier() {
-    assert!(parse("_a").is_err());
-    assert!(parse("0a").is_err())
-}
-
-macro_rules! test_BinOp {
-    ($x:expr, $op:expr) => {
-        assert_eq!(parse($x), Ok(Expression::BinOp($op, Box::new(Expression::Number(0)), Box::new(Expression::Number(0)))))
+    #[test]
+    fn number() {
+        assert_eq!(parse("0"), Ok(Expression::Number(0)))
     }
-}
 
-#[test]
-fn test_operator_add() {
-    test_BinOp!("0+0", Operator::Add)
-}
+    #[test]
+    fn multidigit_number() {
+        assert_eq!(parse("10"), Ok(Expression::Number(10)))
+    }
 
-#[test]
-fn test_operator_sub() {
-    test_BinOp!("0-0", Operator::Sub)
-}
+    #[test]
+    fn identifier() {
+        assert_eq!(parse("abc"), Ok(Expression::Identifier("abc".to_string())))
+    }
 
-#[test]
-fn test_operator_mul() {
-    test_BinOp!("0*0", Operator::Mul)
-}
+    #[test]
+    fn onechar_identifier() {
+        assert_eq!(parse("a"), Ok(Expression::Identifier("a".to_string())))
+    }
 
-#[test]
-fn test_operator_div() {
-    test_BinOp!("0/0", Operator::Div)
+    #[test]
+    fn invalid_identifier() {
+        assert!(parse("_a").is_err());
+        assert!(parse("0a").is_err())
+    }
+
+    macro_rules! BinOp {
+        ($x:expr, $op:expr) => {
+            assert_eq!(parse($x), Ok(Expression::BinOp($op, Box::new(Expression::Number(0)), Box::new(Expression::Number(0)))))
+        }
+    }
+
+    #[test]
+    fn operator_add() {
+        BinOp!("0+0", Operator::Add)
+    }
+
+    #[test]
+    fn operator_sub() {
+        BinOp!("0-0", Operator::Sub)
+    }
+
+    #[test]
+    fn operator_mul() {
+        BinOp!("0*0", Operator::Mul)
+    }
+
+    #[test]
+    fn operator_div() {
+        BinOp!("0/0", Operator::Div)
+    }
 }
