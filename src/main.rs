45b20c8bbefa050649e5047e0071ad87c8fdde25
coord.e <me@coord-e.com>
2018-08-07 16:15:18 +0900

    Add: Add a small arithmetic parser

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+extern crate expressi;
+
 mod syntax {
     include!(concat!(env!("OUT_DIR"), "/syntax.rs"));
 }
 
 fn main() {
-    match syntax::word("internal") {
+    match syntax::expression("1+1") {
         Ok(r) => println!("Parsed as: {:?}", r),
         Err(e) => println!("Parse error: {}", e),
     }
