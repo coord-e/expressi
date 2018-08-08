07da319d7ddf43ac986cbd4e4d82f649f92aa25b
coord.e <me@coord-e.com>
2018-08-08 17:43:19 +0900

    Add: Add assignment operator

--- src/expression.rs
+++ src/expression.rs
@@ -23,5 +23,6 @@ impl Operator {
 pub enum Expression {
     Number(i64),
     Identifier(String),
+    Assignment(Box<Expression>, Box<Expression>),
     BinOp(Operator, Box<Expression>, Box<Expression>),
 }
