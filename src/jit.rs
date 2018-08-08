9832e4ac025524569d7117dcc3830142b5fa10e4
coord.e <me@coord-e.com>
2018-08-08 16:35:33 +0900

    Change: Remove comments came from sample code

--- src/jit.rs
+++ src/jit.rs
@@ -7,29 +7,16 @@ use cranelift::prelude::*;
 use cranelift_module::{DataContext, Linkage, Module};
 use cranelift_simplejit::{SimpleJITBackend, SimpleJITBuilder};
 
-/// The basic JIT class.
 pub struct JIT {
-    /// The function builder context, which is reused across multiple
-    /// FunctionBuilder instances.
     builder_context: FunctionBuilderContext<Variable>,
-
-    /// The main Cranelift context, which holds the state for codegen. Cranelift
-    /// separates this from `Module` to allow for parallel compilation, with a
-    /// context per thread, though this isn't in the simple demo here.
     ctx: codegen::Context,
-
-    /// The data context, which is to data objects what `ctx` is to functions.
     data_ctx: DataContext,
-
-    /// The module, with the simplejit backend, which manages the JIT'd
-    /// functions.
     module: Module<SimpleJITBackend>,
 }
 
 impl JIT {
-    /// Create a new `JIT` instance.
     pub fn new() -> Self {
-        // Windows calling conventions are not supported yet.
+        // Windows calling conventions are not supported by cranelift yet.
         if cfg!(windows) {
             unimplemented!();
         }
@@ -46,29 +33,18 @@ impl JIT {
 
     /// Compile a string in the toy language into machine code.
     pub fn compile(&mut self, name: &str, input: &str) -> Result<*const u8, String> {
-        // First, parse the string, producing AST nodes.
+        // Parse the string, producing AST nodes.
         let ast = parser::parse(&input)
             .map_err(|e| e.to_string())?;
 
-        // Then, translate the AST nodes into Cranelift IR.
+        // Translate the AST nodes into Cranelift IR.
         self.translate(ast)
             .map_err(|e| e.to_string())?;
 
-        // Next, declare the function to simplejit. Functions must be declared
-        // before they can be called, or defined.
-        //
-        // TODO: This may be an area where the API should be streamlined; should
-        // we have a version of `declare_function` that automatically declares
-        // the function?
         let id = self.module
             .declare_function(&name, Linkage::Export, &self.ctx.func.signature)
             .map_err(|e| e.to_string())?;
 
-        // Define the function to simplejit. This finishes compilation, although
-        // there may be outstanding relocations to perform. Currently, simplejit
-        // cannot finish relocations until all functions to be called are
-        // defined. For this toy demo for now, we'll just finalize the function
-        // below.
         self.module
             .define_function(id, &mut self.ctx)
             .map_err(|e| e.to_string())?;
@@ -76,8 +52,7 @@ impl JIT {
         // Now that compilation is finished, we can clear out the context state.
         self.module.clear_context(&mut self.ctx);
 
-        // Finalize the function, finishing any outstanding relocations. The
-        // result is a pointer to the finished machine code.
+        // Finalize the function, finishing any outstanding relocations.
         let code = self.module.finalize_function(id);
 
         Ok(code)
@@ -148,7 +123,6 @@ impl<'a> FunctionTranslator<'a> {
             }
 
             Expression::Identifier(name) => {
-                // `use_var` is used to read the value of a variable.
                 let variable = self.variables.get(&name).expect("variable not defined");
                 self.builder.use_var(*variable)
             }
