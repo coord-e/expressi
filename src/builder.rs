7f89ab330500ca3743eaf9ed9f8fa985d17caa49
coord.e <me@coord-e.com>
2018-08-11 22:02:29 +0900

    Add: Add a constant conversion method

--- src/builder.rs
+++ src/builder.rs
@@ -10,6 +10,14 @@ impl<T> Builder<T> {
         self.inst_builder
     }
 
+    pub fn constant<T>(&self, t: types::Type, v: T) -> Option<Value> {
+        Some(Value::new(match t {
+            types::I64 => self.inst_builder.ins().iconst(t, i64::from(v)),
+            types::B1  => self.inst_builder.ins().bconst(t, v),
+            _ => return None
+        }, t))
+    }
+
     pub fn add(&self, lhs: Value, rhs: Value) -> Value {
         let res = self.inst_builder.ins().iadd(lhs.cl_value(), rhs.cl_value());
         Value::new(res, types::I64)
