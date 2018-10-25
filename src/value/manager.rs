1cd44dc9687a3369f5a96147be469165e056cc37
coord.e <me@coord-e.com>
2018-10-25 22:42:30 +0900

    Add: Add a basic interface to interact with stores

--- src/value/manager.rs
+++ src/value/manager.rs
@@ -5,3 +5,21 @@ pub struct ValueManager {
     type_store: TypeStore,
     value_store: ValueStore
 }
+
+impl ValueManager {
+    pub fn new() -> Self {
+        Self {
+            type_store: TypeStore::new(),
+            value_store: ValueStore::new()
+        }
+    }
+
+    pub fn new_user_type(&mut self, data: EnumTypeData) -> TypeID {
+        self.type_store.new_enum(data)
+    }
+
+    pub fn new_value(&mut self, t: TypeID, data: ValueData) -> ValueID {
+        self.value_store.new_value(t, data)
+    }
+
+}
