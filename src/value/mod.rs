7be0df446b8cdac553d73df86371c62a811f0190
coord.e <me@coord-e.com>
2018-10-25 22:13:28 +0900

    [WIP] Reconsider the design of Value (at least renamed to ValueID)

--- src/value/mod.rs
+++ src/value/mod.rs
@@ -2,6 +2,6 @@ pub mod value;
 pub mod type_;
 pub mod atom;
 
-pub use value::value::{Value, ValueStore, ValueData};
+pub use value::value::{ValueID, ValueStore, ValueData};
 pub use value::type_::{TypeStore, TypeID};
 pub use value::atom::Atom;
--- src/value/value.rs
+++ src/value/value.rs
@@ -7,27 +7,26 @@ use inkwell::values::{BasicValueEnum, PointerValue};
 use inkwell::types::BasicTypeEnum;
 
 #[derive(Debug)]
-pub enum ValueData {
-    Primitive { internal_value: BasicValueEnum, value_type: Type },
-    Array { addr: PointerValue, elements: Vec<Value>, item_type: Type },
+enum ValueDataInternal {
+    Primitive { internal_value: BasicValueEnum },
+    Array { addr: PointerValue, elements: Vec<ValueID> },
     Empty
 }
 
+#[derive(Debug)]
+pub struct ValueData(TypeID, ValueDataInternal)
+
 impl ValueData {
-    pub fn get_type(&self) -> Type {
-        match *self {
-            ValueData::Primitive{value_type, ..} => value_type,
-            ValueData::Array{ref elements, item_type, ..} => Type::Array(Box::into_raw_non_null(Box::new(item_type)), elements.len()),
-            ValueData::Empty => Type::Empty
-        }
+    pub fn get_type(&self) -> TypeID {
+        let ValueData(type_id, ..) = self;
+        return type_id;
     }
 
-    pub fn primitive<V>(v: V, t: Type) -> Self
+    pub fn primitive<V>(v: V, t: TypeID) -> Self
         where BasicValueEnum: From<V> {
-        ValueData::Primitive {
+        ValueData(t, ValueDataInternal::Primitive {
             internal_value: BasicValueEnum::from(v),
-            value_type: t
-        }
+        })
     }
 
     pub fn from_cl<V, T>(v: V, t: T) -> Result<Self, Error>
@@ -38,7 +37,7 @@ impl ValueData {
         })
     }
 
-    pub fn array(addr: PointerValue, elements: Vec<Value>, item_type: Type) -> Self {
+    pub fn array(addr: PointerValue, elements: Vec<ValueID>, item_type: Type) -> Self {
         ValueData::Array {
             addr, elements, item_type
         }
@@ -52,28 +51,31 @@ impl ValueData {
     }
 }
 
+/// The lightweight and copyable reference to ValueData
+#[derive(Clone, Copy, Debug)]
+pub struct ValueID(usize);
+
 /// Stores ValueData
 #[derive(Debug)]
 pub struct ValueStore {
-    data: Vec<ValueData>
+    data: HashMap<ValueID, ValueData>
 }
 
 impl ValueStore {
     pub fn new() -> Self {
         ValueStore {
-            data: Vec::new(),
+            data: HashMap::new(),
         }
     }
 
-    pub fn new_value(&mut self, data: ValueData) -> Value {
-        let t = data.get_type();
-        self.data.push(data);
-        Value::from_idx(self.data.len() - 1, t)
+    pub fn new_value(&mut self, data: ValueData) -> ValueID {
+        let id = ValueID(self.data.len());
+        self.data.insert(id, data);
+        id
     }
 
-    pub fn get(&self, rf: Value) -> Option<&ValueData> {
-        let Value(idx, ..) = rf;
-        if self.data.len() <= idx { None } else { Some(&self.data[idx]) }
+    pub fn get(&self, id: ValueID) -> Option<&ValueData> {
+        self.data.get(id)
     }
 
     pub fn release(&mut self) {
@@ -81,17 +83,3 @@ impl ValueStore {
     }
 }
 
-
-/// The lightweight and copyable reference to ValueData
-#[derive(Clone, Copy, Debug)]
-pub struct Value(usize, Type);
-
-impl Value {
-    fn from_idx(idx: usize, t: Type) -> Self {
-        Value(idx, t)
-    }
-
-    pub fn get_type(&self) -> Type {
-        self.1
-    }
-}
