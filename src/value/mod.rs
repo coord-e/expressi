2988343add8f669b85ddebf6422a12bef7b11964
coord.e <me@coord-e.com>
2018-10-25 21:47:10 +0900

    Change: Move TypeStore things to type_.rs

--- src/value/mod.rs
+++ src/value/mod.rs
@@ -1,9 +1,7 @@
 pub mod value;
 pub mod type_;
-pub mod type_store;
 pub mod atom;
 
-pub use value::type_::Type;
 pub use value::value::{Value, ValueStore, ValueData};
-pub use value::type_store::{TypeStore, TypeID};
+pub use value::type_::{TypeStore, TypeID};
 pub use value::atom::Atom;
--- src/value/type_.rs
+++ src/value/type_.rs
@@ -8,23 +8,29 @@ use std::fmt;
 use std::ptr::NonNull;
 use std::str::FromStr;
 
-#[derive(Debug, Clone, PartialEq, Eq, Copy)]
-pub enum Type {
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct TypeID(usize);
+
+pub type EnumTypeData = Vec<(String, Vec<TypeID>)>;
+
+#[derive(Debug)]
+pub enum TypeData {
     Number,
     Boolean,
-    Array(NonNull<Type>, usize),
+    Array(NonNull<TypeData>, usize),
     Empty,
+    Enum(EnumTypeData)
 }
 
 unsafe impl Send for Type {}
 unsafe impl Sync for Type {}
 
-impl Type {
+impl TypeData {
     pub fn from_cl(t: BasicTypeEnum) -> Result<Self, LLVMTypeConversionError> {
         Ok(match t {
             BasicTypeEnum::IntType(int) => match int.get_bit_width() {
-                1  => Type::Boolean,
-                64 => Type::Number,
+                1  => TypeData::Boolean,
+                64 => TypeData::Number,
                 _  => unimplemented!()
             },
             _ => return Err(LLVMTypeConversionError { from: format!("{:?}", t) }),
@@ -33,29 +39,29 @@ impl Type {
 
     pub fn cl_type(&self) -> Result<BasicTypeEnum, InternalTypeConversionError> {
         Ok(match self {
-            Type::Number => IntType::i64_type(),
-            Type::Boolean => IntType::bool_type(),
+            TypeData::Number => IntType::i64_type(),
+            TypeData::Boolean => IntType::bool_type(),
             _ => return Err(InternalTypeConversionError { from: *self }),
         }.into())
     }
 
     pub fn size(&self) -> usize {
         match self {
-            Type::Number => 8,
-            Type::Boolean => 1,
-            Type::Array(t, length) => unsafe {*t.as_ptr()}.size() * length,
-            Type::Empty => 0
+            TypeData::Number => 8,
+            TypeData::Boolean => 1,
+            TypeData::Array(t, length) => unsafe {*t.as_ptr()}.size() * length,
+            TypeData::Empty => 0
         }
     }
 }
 
-impl fmt::Display for Type {
+impl fmt::Display for TypeData {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let rep: String = match self {
-            Type::Number => "Number".to_string(),
-            Type::Boolean => "Boolean".to_string(),
-            Type::Array(t, length) => format!("[{}; {}]", unsafe {*t.as_ptr()}, length),
-            Type::Empty => "Empty".to_string(),
+            TypeData::Number => "Number".to_string(),
+            TypeData::Boolean => "Boolean".to_string(),
+            TypeData::Array(t, length) => format!("[{}; {}]", unsafe {*t.as_ptr()}, length),
+            TypeData::Empty => "Empty".to_string(),
         };
 
         write!(f, "{}", rep)
@@ -65,16 +71,37 @@ impl fmt::Display for Type {
 #[derive(Debug, Clone, PartialEq)]
 pub struct TypeParseError;
 
-impl FromStr for Type {
+impl FromStr for TypeData {
     type Err = TypeParseError;
 
     fn from_str(x: &str) -> Result<Self, Self::Err> {
         Ok(match x {
-            "Number" => Type::Number,
-            "Boolean" => Type::Boolean,
-            "Empty" => Type::Empty,
+            "Number" => TypeData::Number,
+            "Boolean" => TypeData::Boolean,
+            "Empty" => TypeData::Empty,
             _ => return Err(TypeParseError),
         })
     }
 }
 
+pub struct TypeStore {
+    types: HashMap<TypeID, TypeData>
+}
+
+impl TypeStore {
+    pub fn new() -> Self {
+        let mut store = Self { types: HashMap::new() };
+        store.new_type(TypeData::Number);
+        store
+    }
+
+    fn new_type(&mut self, data: TypeData) -> TypeID {
+        let id = TypeID(self.types.len());
+        self.types.insert(id, data);
+        id
+    }
+
+    pub fn new_enum(&mut self, data: EnumTypeData) -> TypeID {
+        self.new_type(TypeData::Enum(data))
+    }
+}
