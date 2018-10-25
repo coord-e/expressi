48dc915a633c3a44d1166c7148fe42b38693573b
coord.e <me@coord-e.com>
2018-10-25 22:29:08 +0900

    [WIP] Add a manager class to manage ValueStore and TypeStore

---
+++ src/value/manager.rs
@@ -0,0 +1,7 @@
+use value::type_::TypeStore;
+use value::value::ValueStore;
+
+pub struct ValueManager {
+    type_store: TypeStore,
+    value_store: ValueStore
+}
--- src/value/mod.rs
+++ src/value/mod.rs
@@ -1,7 +1,9 @@
 pub mod value;
 pub mod type_;
+pub mod manager;
 pub mod atom;
 
 pub use value::value::{ValueID, ValueStore, ValueData};
 pub use value::type_::{TypeStore, TypeID};
 pub use value::atom::Atom;
+pub use value::manager::ValueManager;
