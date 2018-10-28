bfa96336f13cb2a6963ce66ca8a4456b33f6237a
coord.e <me@coord-e.com>
2018-10-29 01:57:13 +0900

    Delete: Delete obsolete API in TypedValueData

--- src/value/value.rs
+++ src/value/value.rs
@@ -24,27 +24,6 @@ impl TypedValueData {
         return type_id;
     }
 
-    pub fn primitive<V>(v: V, t: TypeID) -> Self
-        where BasicValueEnum: From<V> {
-        TypedValueData(t, ValueData::Primitive {
-            internal_value: BasicValueEnum::from(v),
-        })
-    }
-
-    pub fn from_cl<V, T>(v: V, t: T) -> Result<Self, Error>
-        where BasicValueEnum: From<V>, BasicTypeEnum: From<T> {
-        Ok(ValueData::Primitive {
-            internal_value: BasicValueEnum::from(v),
-            value_type: Type::from_cl(BasicTypeEnum::from(t))?
-        })
-    }
-
-    pub fn array(addr: PointerValue, elements: Vec<ValueID>, item_type: Type) -> Self {
-        ValueData::Array {
-            addr, elements, item_type
-        }
-    }
-
     pub fn cl_value(&self) -> Result<BasicValueEnum, Error> {
         Ok(match *self {
             ValueData::Primitive {internal_value, ..} => internal_value,
