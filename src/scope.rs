65130d81b3f9164f296608903c91068a77d7a564
coord.e <me@coord-e.com>
2018-11-02 16:24:12 +0900

    Change: Fix the methods in Scope

--- src/scope.rs
+++ src/scope.rs
@@ -1,5 +1,5 @@
 use error::UnexpectedScopePopError;
-use value::{TypeID, ValueID, ValueManagerRef};
+use value::{Atom, TypeID, ValueID, ValueManagerRef};
 
 use std::collections::HashMap;
 use std::rc::Rc;
@@ -8,75 +8,61 @@ use failure::Error;
 
 use inkwell::values::PointerValue;
 
-type VariableId = usize;
+pub enum BindingKind {
+    Mutable,
+    Immutable
+}
+
+struct BoundAtom {
+    kind: BindingKind,
+    atom: Atom
+}
 
 pub struct Scope {
-    variables: HashMap<String, VariableId>,
-    variable_values: HashMap<VariableId, ValueID>,
-    variable_pointers: HashMap<VariableId, PointerValue>,
+    variable_pointers: HashMap<String, PointerValue>,
     manager: ValueManagerRef,
-    types: HashMap<String, TypeID>,
+    bindings: HashMap<String, BoundAtom>,
 }
 
 impl Scope {
     pub fn new(manager: ValueManagerRef) -> Self {
         Scope {
-            variables: HashMap::new(),
-            variable_values: HashMap::new(),
             variable_pointers: HashMap::new(),
-            types: HashMap::new(),
+            bindings: HashMap::new(),
             manager,
         }
     }
 
-    pub fn get(&self, s: &str) -> Option<ValueID> {
-        self.variables
+    pub fn get(&self, s: &str) -> Option<Atom> {
+        self.bindings
             .get(s)
-            .and_then(|var| self.variable_values.get(var))
-            .cloned()
+            .map(|b| b.atom)
     }
 
     pub fn get_var(&self, s: &str) -> Option<PointerValue> {
-        self.variables
+        self.variable_pointers
             .get(s)
-            .and_then(|var| self.variable_pointers.get(var))
             .cloned()
     }
 
-    pub fn set(&mut self, s: &str, val: ValueID) {
-        self.variable_values
-            .insert(*self.variables.get(s).unwrap(), val);
+    pub fn bind(&mut self, s: &str, atom: Atom, kind: BindingKind) {
+        self.bindings.insert(s.to_string(), BoundAtom { kind, atom });
     }
 
-    pub fn add(&mut self, s: &str, val: ValueID, var: PointerValue) {
-        let idx = self.variables.len();
-        self.variables.insert(s.to_string(), idx);
-        self.variable_values.insert(idx, val);
-        self.variable_pointers.insert(idx, var);
+    pub fn add_var(&mut self, s: &str, var: PointerValue) {
+        self.variable_pointers.insert(s.to_string(), var);
     }
 
     pub fn variables(&self) -> impl Iterator<Item = (&String, PointerValue)> {
-        self.variables
+        self.variable_pointers
             .iter()
-            .map(move |(k, v)| (k, self.variable_pointers.get(&v).cloned().unwrap()))
+            .map(|(k, v)| (k, v.clone()))
     }
 
-    pub fn values(&self) -> impl Iterator<Item = (&String, ValueID)> {
-        self.variables
+    pub fn bindings(&self) -> impl Iterator<Item = (&String, Atom)> {
+        self.bindings
             .iter()
-            .map(move |(k, v)| (k, self.variable_values.get(&v).unwrap().clone()))
-    }
-
-    pub fn types(&self) -> impl Iterator<Item = (&String, TypeID)> {
-        self.types.iter().map(|(k, v)| (k, v.clone()))
-    }
-
-    pub fn resolve_type(&self, id: &str) -> Option<TypeID> {
-        self.types.get(id).cloned()
-    }
-
-    pub fn add_type(&mut self, name: &str, id: TypeID) {
-        self.types.insert(name.to_string(), id);
+            .map(|(k, v)| (k, v.atom))
     }
 }
 
@@ -112,44 +98,32 @@ impl ScopeStack {
         self.scopes.iter().flat_map(|it| it.variables())
     }
 
-    pub fn values(&self) -> impl Iterator<Item = (&String, ValueID)> {
-        self.scopes.iter().flat_map(|it| it.values())
+    pub fn bindings(&self) -> impl Iterator<Item = (&String, Atom)> {
+        self.scopes.iter().flat_map(|it| it.bindings())
     }
 
-    pub fn types(&self) -> impl Iterator<Item = (&String, TypeID)> {
-        self.scopes.iter().flat_map(|it| it.types())
+    pub fn add_var(&mut self, s: &str, var: PointerValue) {
+        self.scopes.last_mut().unwrap().add_var(s, var)
     }
 
-    pub fn add(&mut self, s: &str, val: ValueID, var: PointerValue) {
-        self.scopes.last_mut().unwrap().add(s, val, var)
-    }
-
-    pub fn get(&self, s: &str) -> Option<ValueID> {
-        self.values().find(|(k, _)| k == &s).map(|(_, v)| v)
+    pub fn get(&self, s: &str) -> Option<Atom> {
+        self.bindings().find(|(k, _)| k == &s).map(|(_, v)| v)
     }
 
     pub fn get_var(&self, s: &str) -> Option<PointerValue> {
         self.variables().find(|(k, _)| k == &s).map(|(_, v)| v)
     }
 
-    pub fn set(&mut self, s: &str, val: ValueID) {
+    pub fn bind(&mut self, s: &str, val: Atom, kind: BindingKind) {
         let mut it = self.scopes.iter_mut();
         it.find(|sc| sc.get(s).is_some())
             .or(it.last())
             .unwrap()
-            .set(s, val)
+            .bind(s, val, kind)
     }
 
     pub fn unique_name(&self, s: &str) -> String {
         let num_vars = self.variables().count();
         format!("{}.{}", s, num_vars)
-    }
-
-    pub fn resolve_type(&self, id: &str) -> Option<TypeID> {
-        self.types().find(|(k, _)| k == &id).map(|(_, v)| v)
-    }
-
-    pub fn add_type(&mut self, name: &str, id: TypeID) {
-        self.scopes.last_mut().unwrap().add_type(name, id);
     }
 }
