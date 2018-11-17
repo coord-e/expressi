8e1abf43cb894e3877015110613b2536b23bf390
coord.e <me@coord-e.com>
2018-11-18 00:40:37 +0900

    Fix: Fix many basic mistakes to pass compile

--- src/translator/ast_translator.rs
+++ src/translator/ast_translator.rs
@@ -14,19 +14,35 @@ impl ASTTranslator {
             Expression::BinOp(op, lhs, rhs) => {
                 let lhs = self.translate_expr(*lhs);
                 let rhs = self.translate_expr(*rhs);
-                Value::BinOp(op, lhs, rhs)
+                Value::BinOp(op, Box::new(lhs), Box::new(rhs))
             }
-            Expression::Follow(lhs, rhs) => Value::Follow(lhs, rhs),
-            Expression::Bind(kind, name, rhs) => Value::Bind(kind, name, rhs),
-            Expression::Assign(lhs, rhs) => Value::Assign(lhs, rhs),
+            Expression::Follow(lhs, rhs) => {
+                let rhs_value = self.translate_expr(*rhs);
+                let lhs_value = self.translate_expr(*lhs);
+                Value::Follow(Box::new(lhs_value), Box::new(rhs_value))
+            }
+            Expression::Bind(kind, name, rhs) => {
+                let rhs_value = self.translate_expr(*rhs);
+                Value::Bind(kind, name, Box::new(rhs_value))
+            }
+            Expression::Assign(lhs, rhs) => {
+                let rhs_value = self.translate_expr(*rhs);
+                let lhs_value = self.translate_expr(*lhs);
+                Value::Assign(Box::new(lhs_value), Box::new(rhs_value))
+            }
             Expression::TypeIdentifier(id) => unimplemented!(),
             Expression::Identifier(name) => Value::Variable(name),
             Expression::Cast(lhs, rhs) => unimplemented!(),
             Expression::Scope(expr) => {
                 let content = self.translate_expr(*expr);
-                Value::Scope(content)
+                Value::Scope(Box::new(content))
             }
-            Expression::IfElse(cond, then_expr, else_expr) => Value::IfElse(cond, then_expr, else_expr)
+            Expression::IfElse(cond_expr, then_expr, else_expr) => {
+                let cond_value = self.translate_expr(*cond_expr);
+                let then_value = self.translate_expr(*then_expr);
+                let else_value = self.translate_expr(*else_expr);
+                Value::IfElse(Box::new(cond_value), Box::new(then_value), Box::new(else_value))
+            }
         }
     }
 }
