b38542e453e74f83d7f4625d31f9a0ead92cca4a
coord.e <me@coord-e.com>
2018-11-17 23:36:16 +0900

    Add: Add more entities

--- src/ir/eir.rs
+++ src/ir/eir.rs
@@ -1,12 +1,20 @@
+use expression::Operator;
+
 pub enum Constant {
     Number(i64),
     Boolean(bool),
     Empty
 }
 
+pub type Identifier = String;
+
 pub enum Value {
     Bind(Identifier, Box<Value>),
     Assign(Box<Value>, Box<Value>),
+    Scope(Box<Value>),
+    Follow(Box<Value>, Box<Value>),
+    BinOp(Operator, Box<Value>, Box<Value>),
+    Variable(Identifier),
     Constant(Constant)
 }
 
