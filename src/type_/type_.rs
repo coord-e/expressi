ca3ddaa20f74f34e972940661c13935de8e00130
coord.e <me@coord-e.com>
2018-11-20 02:42:43 +0900

    Delete: Delete unused implementations to make things easier

--- src/type_/type_.rs
+++ src/type_/type_.rs
@@ -1,7 +1,3 @@
-use error::TranslationError;
-
-use failure::Error;
-use inkwell::types::{BasicTypeEnum, IntType};
 use type_::TypeID;
 
 use std::fmt;
@@ -19,49 +15,6 @@ pub enum TypeData {
     Enum(EnumTypeData),
     Variable(Option<TypeID>),
     PolyVariable(Vec<TypeID>),
-}
-
-impl TypeData {
-    pub fn from_cl(t: BasicTypeEnum) -> Result<Self, Error> {
-        Ok(match t {
-            BasicTypeEnum::IntType(int) => match int.get_bit_width() {
-                1 => TypeData::Boolean,
-                64 => TypeData::Number,
-                _ => unimplemented!(),
-            },
-            _ => {
-                return Err(TranslationError::LLVMTypeConversion {
-                    from: format!("{:?}", t),
-                }.into())
-            }
-        })
-    }
-
-    pub fn cl_type(&self) -> Result<BasicTypeEnum, Error> {
-        Ok(match self {
-            TypeData::Number => IntType::i64_type(),
-            TypeData::Boolean => IntType::bool_type(),
-            _ => {
-                return Err(TranslationError::InternalTypeConversion {
-                    type_description: format!("{:?}", self),
-                }.into())
-            }
-        }.into())
-    }
-
-    pub fn size(&self) -> usize {
-        match self {
-            TypeData::Number => 8,
-            TypeData::Boolean => 1,
-            TypeData::Array(_, _) => unimplemented!(),
-            // TODO: Architecture-independent pointer size
-            TypeData::Function(_, _) => 8,
-            TypeData::Empty => 0,
-            TypeData::Enum(_) => unimplemented!(),
-            TypeData::Variable(_) => unimplemented!(),
-            TypeData::PolyVariable(_) => unimplemented!(),
-        }
-    }
 }
 
 impl fmt::Display for TypeData {
