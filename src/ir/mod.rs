5d33dc547571e335e8a88d4637b3d2f348ec0c22
coord.e <me@coord-e.com>
2018-11-20 01:38:07 +0900

    [WIP] Add: Add a printer of EIR

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -1,3 +1,5 @@
 pub mod eir;
+pub mod printer;
 
 pub use ir::eir::{BindingKind, Constant, Value};
+pub use ir::printer::Printer;
---
+++ src/ir/printer.rs
@@ -0,0 +1,70 @@
+use type_::TypeStore;
+use ir::{Value, Constant};
+
+use std::fmt;
+
+pub struct Printer<'a> {
+    type_store: &'a TypeStore
+}
+
+impl<'a> Printer<'a> {
+    pub fn print(&self, v: &Value, f: &mut fmt::Formatter) -> fmt::Result {
+        match v {
+            Value::Constant(c) => match c {
+                Constant::Number(number) => write!(f, "{}", number),
+                Constant::Boolean(tf) => write!(f, "{}", tf),
+                Constant::Empty => write!(f, "<empty>"),
+            }
+            Value::BinOp(op, lhs, rhs) => {
+                self.print(lhs, f)?;
+                write!(f, " {:?} ", op)?;
+                self.print(rhs, f)
+            }
+
+            Value::Follow(lhs, rhs) => {
+                self.print(lhs, f)?;
+                write!(f, ";\n")?;
+                self.print(rhs, f)
+            }
+
+            Value::Bind(kind, name, rhs) => {
+                write!(f, "let {} {} = ", kind, name)?;
+                self.print(rhs, f)
+            }
+
+            Value::Assign(lhs, rhs) => {
+                self.print(lhs, f)?;
+                write!(f, " = ")?;
+                self.print(rhs, f)
+            }
+
+            Value::Variable(name) => write!(f, "{}", name),
+            Value::Scope(expr) => {
+                write!(f, "{{ ")?;
+                self.print(expr, f)?;
+                write!(f, " }}")
+            }
+            Value::IfElse(cond, then_expr, else_expr) => {
+                self.print(cond, f)?;
+                write!(f, " ? ")?;
+                self.print(then_expr, f)?;
+                write!(f, " : ")?;
+                self.print(else_expr, f)
+            }
+            Value::Apply(func, arg) => {
+                self.print(func, f)?;
+                write!(f, "(")?;
+                self.print(arg, f)?;
+                write!(f, ")")
+            }
+            Value::Function(param, body) => {
+                write!(f, "{} -> ", param)?;
+                self.print(body, f)
+            }
+            Value::Typed(ty, val) => {
+                self.print(val, f)?;
+                write!(f, " :: {:?}", self.type_store.get(*ty).unwrap())
+            }
+        }
+    }
+}
