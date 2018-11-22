06e8b8098cd9a167d99c4e827cff9618e1d53a90
coord.e <me@coord-e.com>
2018-11-22 15:48:34 +0900

    Delete: Delete TypeID and TypeStore

--- src/type_/mod.rs
+++ src/type_/mod.rs
@@ -1,6 +1,3 @@
-pub mod primitive;
-pub mod store;
 pub mod type_;
 
-pub use type_::primitive::PrimitiveKind;
-pub use type_::store::{TypeID, TypeStore};
+pub use type_::type_::Type;
--- src/type_/primitive.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Debug, PartialEq, Eq, Hash)]
-pub enum PrimitiveKind {
-    Number,
-    Boolean,
-    Empty,
-}
