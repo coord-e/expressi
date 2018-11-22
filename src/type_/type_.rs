53a762c5a3004e6bcf18f90fa60c85fed50bd3f8
coord.e <me@coord-e.com>
2018-11-22 12:52:40 +0900

    Add: Implement fmt::Display for Type

--- src/type_/type_.rs
+++ src/type_/type_.rs
@@ -23,20 +23,12 @@ pub enum Type {
     Function(Box<Type>, Box<Type>)
 }
 
-impl fmt::Display for TypeData {
+impl fmt::Display for Type {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let rep: String = match self {
-            TypeData::Operator(kind, tys) => match kind {
-                OperatorKind::Number => "Number".to_string(),
-                OperatorKind::Boolean => "Boolean".to_string(),
-                OperatorKind::Empty => "Empty".to_string(),
-                OperatorKind::Function => format!("{:?} -> {:?}", tys[0], tys[1]),
-            },
-            TypeData::Enum(data) => format!("{:?}", data),
-            TypeData::Variable(instance) => format!("var({:?})", instance),
-            TypeData::PolyVariable(types) => format!("pvar({:?})", types),
-        };
-
-        write!(f, "{}", rep)
+        match self {
+            Type::Variable(id) => write!(f, "{}", id),
+            Type::Function(box t1, box t2) => write!(f, "({} -> {})", t1, t2),
+            _ => write!(f, "{:?}", self)
+        }
     }
 }
