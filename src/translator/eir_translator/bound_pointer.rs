5b99529d0ffb6d2df6aabb459beadef8347f385a
coord.e <me@coord-e.com>
2018-11-25 22:51:39 +0900

    [WIP] Change: Use Atom<PointerValue> for the base of binding

--- src/translator/eir_translator/bound_pointer.rs
+++ src/translator/eir_translator/bound_pointer.rs
@@ -1,19 +1,20 @@
 use ir::BindingKind;
+use translator::eir_translato::atom::Atom;
 
 use inkwell::values;
 
 #[derive(Debug, Clone)]
 pub struct BoundPointer {
     kind: BindingKind,
-    ptr: values::PointerValue,
+    ptr: Atom<values::PointerValue>,
 }
 
 impl BoundPointer {
-    pub fn new(kind: BindingKind, ptr: values::PointerValue) -> Self {
+    pub fn new(kind: BindingKind, ptr: Atom<values::PointerValue>) -> Self {
         Self { kind, ptr }
     }
 
-    pub fn ptr_value(&self) -> values::PointerValue {
+    pub fn ptr_value(&self) -> Atom<values::PointerValue> {
         self.ptr
     }
 
