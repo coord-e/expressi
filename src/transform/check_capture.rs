9fa0918ca69ef3c72ddb468bf9739455a20efab7
coord.e <me@coord-e.com>
2018-12-16 14:51:32 +0900

    Fix: Handle errors correctly in CheckCapture

--- src/transform/check_capture.rs
+++ src/transform/check_capture.rs
@@ -1,6 +1,7 @@
 use ir;
 use transform::type_infer::Type;
 use transform::Transform;
+use transform::error::CheckCaptureError;
 
 use failure::Error;
 
@@ -8,30 +9,30 @@ use std::collections::HashMap;
 
 pub struct CheckCapture;
 
-fn collect_vars(eir: &ir::Value) -> Box<dyn Iterator<Item = (ir::Identifier, Type)>> {
-    match eir {
+fn collect_vars(eir: &ir::Value) -> Result<Box<dyn Iterator<Item = (ir::Identifier, Type)>>, Error> {
+    Ok(match eir {
         ir::Value::Typed(ty, _, box value) => match value {
             ir::Value::Variable(ident) => box vec![(ident.clone(), ty.clone())].into_iter(),
             ir::Value::Constant(_) => box vec![].into_iter(),
-            ir::Value::Bind(_, _, box v) => collect_vars(v),
-            ir::Value::Assign(box lhs, box rhs) => box collect_vars(lhs).chain(collect_vars(rhs)),
-            ir::Value::Scope(box body) => collect_vars(body),
-            ir::Value::Follow(box lhs, box rhs) => box collect_vars(lhs).chain(collect_vars(rhs)),
-            ir::Value::Apply(box lhs, box rhs) => box collect_vars(lhs).chain(collect_vars(rhs)),
-            ir::Value::BinOp(_, box lhs, box rhs) => box collect_vars(lhs).chain(collect_vars(rhs)),
-            ir::Value::IfElse(box cond, box then_, box else_) => box collect_vars(cond)
-                .chain(collect_vars(then_))
-                .chain(collect_vars(else_)),
+            ir::Value::Bind(_, _, box v) => collect_vars(v)?,
+            ir::Value::Assign(box lhs, box rhs) => box collect_vars(lhs)?.chain(collect_vars(rhs)?),
+            ir::Value::Scope(box body) => collect_vars(body)?,
+            ir::Value::Follow(box lhs, box rhs) => box collect_vars(lhs)?.chain(collect_vars(rhs)?),
+            ir::Value::Apply(box lhs, box rhs) => box collect_vars(lhs)?.chain(collect_vars(rhs)?),
+            ir::Value::BinOp(_, box lhs, box rhs) => box collect_vars(lhs)?.chain(collect_vars(rhs)?),
+            ir::Value::IfElse(box cond, box then_, box else_) => box collect_vars(cond)?
+                .chain(collect_vars(then_)?)
+                .chain(collect_vars(else_)?),
             ir::Value::Function(ident, box body, captures) => {
                 let ident = ident.clone();
-                box collect_vars(body)
+                box collect_vars(body)?
                     .chain(captures.clone().into_iter())
                     .filter(move |(e, _)| *e != ident)
             }
-            ir::Value::Typed(..) => panic!("kok"),
+            ir::Value::Typed(..) => return Err(CheckCaptureError::DoubleTyped.into()),
         },
-        _ => panic!("wo"),
-    }
+        _ => return Err(CheckCaptureError::NotTyped.into()),
+    })
 }
 
 impl CheckCapture {
@@ -50,7 +51,7 @@ impl Transform for CheckCapture {
         Ok(ir::Value::Function(
             ident.clone(),
             box body.clone(),
-            collect_vars(body).filter(|(e, _)| e != ident).collect(),
+            collect_vars(body)?.filter(|(e, _)| e != ident).collect(),
         ))
     }
 }
