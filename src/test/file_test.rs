91d6876cdb3c5b33e0eca086f78c1d01b5920436
coord.e <me@coord-e.com>
2019-03-10 07:49:48 +0000

    Add: Enable to test against sample sources and answers

---
+++ src/test/file_test.rs
@@ -0,0 +1,24 @@
+use crate::jit::JIT;
+
+macro_rules! file_test {
+    ($name: ident) => {
+        #[test]
+        fn $name() {
+            let mut jit = JIT::new(false, false, false).unwrap();
+
+            let contents = include_str!(concat!("test-data/", stringify!($name), ".epi"));
+            match jit.compile("test_input", &contents.trim()) {
+                Ok(func) => {
+                    let result = unsafe { func.call() };
+                    assert_eq!(
+                        result,
+                        include!(concat!("test-data/", stringify!($name), ".ans"))
+                    );
+                }
+                Err(err) => assert!(false, format!("{}", err)),
+            }
+        }
+    };
+}
+
+file_test!(curry);
