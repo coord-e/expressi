bd191616584d96aea9fab8a883739104e021db56
coord.e <me@coord-e.com>
2019-03-18 13:30:15 +0000

    Change: Skip coverage in `cli` and `test` module

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 #![feature(box_syntax, box_patterns, never_type)]
 
+#[cfg_attr(tarpaulin, skip)]
 pub mod cli;
+
+#[cfg_attr(tarpaulin, skip)]
+pub mod test;
+
 pub mod codegen;
 pub mod expression;
 pub mod ir;
 pub mod parser;
 pub mod scope;
-pub mod test;
 pub mod transform;
 pub mod translator;
 
