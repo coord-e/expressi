de1367d4959cd74b76b67b1063cf4bb31f5413d3
coord.e <me@coord-e.com>
2019-03-18 09:15:59 +0000

    Add: Re-export `run` and `build`

--- src/compile/mod.rs
+++ src/compile/mod.rs
@@ -2,3 +2,6 @@ pub mod build;
 pub mod jit;
 pub mod llvm;
 pub mod opts;
+
+pub use build::build;
+pub use jit::run;
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,27 @@
 use ansi_term::Colour::Red;
 use structopt::StructOpt;
 
-use expressi::compile::build;
-use expressi::compile::jit;
-use expressi::compile::opts::{BuildOpt, RunOpt};
+use expressi::compile::{self, opts};
 
 #[derive(StructOpt)]
 enum Opt {
     #[structopt(name = "run")]
     Run {
         #[structopt(flatten)]
-        opt: RunOpt,
+        opt: opts::RunOpt,
     },
     #[structopt(name = "build")]
     Build {
         #[structopt(flatten)]
-        opt: BuildOpt,
+        opt: opts::BuildOpt,
     },
 }
 
 #[cfg_attr(tarpaulin, skip)]
 fn main() {
     let result = match Opt::from_args() {
-        Opt::Run { opt } => jit::run(&opt).map(|_| ()),
-        Opt::Build { opt } => build::build(opt),
+        Opt::Run { opt } => compile::run(&opt).map(|_| ()),
+        Opt::Build { opt } => compile::build(opt),
     };
     if let Err(e) = result {
         eprintln!("{}: {}", Red.paint("Fatal Error"), e);
