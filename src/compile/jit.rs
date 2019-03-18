313c54f0931877615eba61653cfbc2ecb2cf2552
coord.e <me@coord-e.com>
2019-03-18 09:24:17 +0000

    Change: Delete `JIT` class and replace its functionality with `compile_jit`

--- src/compile/jit.rs
+++ src/compile/jit.rs
@@ -18,10 +18,9 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::process;
 
-type CompiledFunc = unsafe extern "C" fn() -> u64;
-
 pub fn run(opt: &RunOpt) -> Result<!, Error> {
-    let mut jit = JIT::new(opt.print_ast, opt.print_eir, opt.print_ir)?;
+    Target::initialize_native(&InitializationConfig::default())
+        .map_err(|message| LLVMError::TargetInitializationFailed { message })?;
 
     if let Some(path) = &opt.input {
         let mut f = File::open(path).map_err(|_| CLIError::NotFound { path: path.clone() })?;
@@ -29,14 +28,14 @@ pub fn run(opt: &RunOpt) -> Result<!, Error> {
         f.read_to_string(&mut contents)
             .map_err(|error| CLIError::IOError { error })?;
 
-        let func = jit.compile("file_input", &contents.trim())?;
+        let func = compile_jit(&contents.trim(), "file_input", opt)?;
         process::exit(unsafe { func.call() } as i32)
     } else {
         let home = dirs::home_dir().unwrap_or_else(|| env::current_dir().unwrap());
         let mut shell = Shell::new(home.join(".expressi_history"));
         loop {
             let line = shell.get_next_line()?;
-            match jit.compile("repl", line.trim()) {
+            match compile_jit(line.trim(), "repl", opt) {
                 Ok(func) => {
                     println!(
                         "{}{}",
@@ -52,62 +51,43 @@ pub fn run(opt: &RunOpt) -> Result<!, Error> {
     }
 }
 
-pub struct JIT {
-    print_ast: bool,
-    print_eir: bool,
-    print_ir: bool,
-}
+type CompiledFunc = unsafe extern "C" fn() -> u64;
 
-impl JIT {
-    pub fn new(print_ast: bool, print_eir: bool, print_ir: bool) -> Result<Self, Error> {
-        Target::initialize_native(&InitializationConfig::default())
-            .map_err(|message| LLVMError::TargetInitializationFailed { message })?;
+pub fn compile_jit(
+    source: &str,
+    module_name: &str,
+    opt: &RunOpt,
+) -> Result<execution_engine::JitFunction<CompiledFunc>, Error> {
+    let ast = parser::parse(&source)?;
 
-        Ok(Self {
-            print_ast,
-            print_eir,
-            print_ir,
-        })
+    if opt.print_ast {
+        eprintln!("AST:\n{:#?}", ast);
     }
 
-    /// Compile a string in the toy language into machine code.
-    pub fn compile(
-        &mut self,
-        name: &str,
-        input: &str,
-    ) -> Result<execution_engine::JitFunction<CompiledFunc>, Error> {
-        // Parse the string, producing AST nodes.
-        let ast = parser::parse(&input)?;
+    let eir = translate_ast(ast)?;
 
-        if self.print_ast {
-            eprintln!("AST:\n{:#?}", ast);
-        }
-
-        let eir = translate_ast(ast)?;
-
-        if self.print_eir {
-            eprintln!("EIR:\n{}\n", eir);
-        }
-
-        let transformed = TransformManager::default().apply(eir)?;
-
-        if self.print_eir {
-            eprintln!("Transformed EIR:\n{}\n", transformed);
-        }
-
-        let result = llvm::compile_eir(transformed, name)?;
-
-        if self.print_ir {
-            eprintln!("LLVM IR: \n{}", result.llvm_ir());
-        }
-
-        result.verify()?;
-
-        let execution_engine = result
-            .module()
-            .create_jit_execution_engine(OptimizationLevel::None)
-            .map_err(|_| LLVMError::FailedToCreateJIT)?;
-
-        unsafe { execution_engine.get_function(name) }.map_err(Into::into)
+    if opt.print_eir {
+        eprintln!("EIR:\n{}\n", eir);
     }
+
+    let transformed = TransformManager::default().apply(eir)?;
+
+    if opt.print_eir {
+        eprintln!("Transformed EIR:\n{}\n", transformed);
+    }
+
+    let result = llvm::compile_eir(transformed, module_name)?;
+
+    if opt.print_ir {
+        eprintln!("LLVM IR: \n{}", result.llvm_ir());
+    }
+
+    result.verify()?;
+
+    let execution_engine = result
+        .module()
+        .create_jit_execution_engine(OptimizationLevel::None)
+        .map_err(|_| LLVMError::FailedToCreateJIT)?;
+
+    unsafe { execution_engine.get_function(module_name) }.map_err(Into::into)
 }
